4de07e1cbb40608cd28872dd0beefdc28f578b3f
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2017-08-28 17:34:13 +0200

    Fix some bugs

--- main.py
+++ main.py
@@ -8,31 +8,37 @@ import json
 from telegram import User, TelegramObject, ChatMember
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "445026185:AAEGtZap8RVXlVmi-9AHB_3_6gxIsJCEBi4"
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+DEBUG = os.getenv("DEBUG", False)
 WELCOME_TPL = os.getenv("WELCOME_TPL", "Welcome {user_name}!")
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = log.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def new_user(bot, update):
     user_name = update.message.from_user.name
-    message_text = WELCOME_TPL.format(user_name)
+    message_text = WELCOME_TPL.format(user_name=user_name)
     bot.sendMessage(chat_id=update.message.chat_id, text=message_text)
-    bot.ChatMember(user_name)
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
     # Message for new users
+    if DEBUG:
+        dp.add_handler(CommandHandler('start', start))
+
     dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_user))
 
     # Ignore all messages until it is called
-    # TODO: Clarify this
     updater.start_polling(clean=True)
 
+    # Wait for termination
     updater.idle()
 
 if __name__ == "__main__":
