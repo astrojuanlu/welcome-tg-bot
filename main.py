d6a92f4e53f70ad520fa488013621e60ffe6ff1d
Schmidt Cristian Hernán <schcriher@gmail.com>
2019-02-09 00:24:56 -0300

    Less intrusive way of greeting

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 PORT = int(os.environ["PORT"])
 
-MSG = "¡Te damos la bienvenida{}! En el mensaje anclado tienes las reglas básicas del grupo."
+
+MSG = "¡Te damos la bienvenida {}! En el mensaje anclado están las reglas básicas del grupo."
+TIMEOUT = 180  # in seconds
 
 URI_MAIL = r'[/@]WORD(\.WORD)'.replace('WORD', '[^\s.]+')
 BAN_RULES = (
@@ -18,6 +20,8 @@ BAN_RULES = (
     (re.compile(URI_MAIL).search, "id={} member in id={} group ban by: name with url/uri/email"),
 )
 NOT_BAN_MSG = "id={} member in id={} group can not ban: {}"
+GREET_FROM_MEMBERS = re.compile("[bv]ien[vb]enid|welcome", re.IGNORECASE).search
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -43,8 +47,36 @@ def ban_member(bot, update, user, reason):
         logger.debug(NOT_BAN_MSG.format(user.id, chat.id, cause))
 
 
-def new_user(bot, update):
-    message_texts = []
+def send_greeting(bot, job):
+    # Users must have first_name, last_name or username to be greeted
+    chat_id, chat_data = job.context
+    user_list = ', '.join(u for u in chat_data['users_to_greet'].values() if u)
+    if user_list:
+        bot.send_message(chat_id, text=MSG.format(user_list))
+        not_greet_user(chat_data)
+
+
+def delete_job(chat_data):
+    if 'job' in chat_data:
+        job = chat_data['job']
+        job.schedule_removal()
+        del chat_data['job']
+
+
+def not_greet_user(chat_data, user_id=None):
+    # user_id=None do not greet anyone
+    if 'users_to_greet' in chat_data:
+        if user_id:
+            chat_data['users_to_greet'].pop(user_id, None)
+        else:
+            chat_data['users_to_greet'].clear()
+
+
+def new_user(bot, update, job_queue, chat_data):
+    if 'users_to_greet' not in chat_data:
+        chat_data['users_to_greet'] = {}
+
+    new_users = False
     for user in (update.message.new_chat_members or [update.message.from_user]):
         name = user.first_name or user.last_name or user.username or ""
         greet = True
@@ -54,25 +86,51 @@ def new_user(bot, update):
                     ban_member(bot, update, user, reason)
                     greet = False
                     break
-            else:
-                name = ", {}".format(name)
         if greet and not user.is_bot:
-            message_texts.append(MSG.format(name))
-    if message_texts:
-        update.message.chat.send_message(text='\n'.join(message_texts))
+            chat_data['users_to_greet'][user.id] = name
+            new_users = True
+
+    if new_users:
+        # Resetting the waiting time
+        delete_job(chat_data)
+        # New countdown
+        job = job_queue.run_once(send_greeting, TIMEOUT, context=(update.message.chat_id, chat_data))
+        chat_data['job'] = job
 
 
-def bye_user(bot, update):
+def bye_user(bot, update, chat_data):
+    not_greet_user(chat_data, update.message.left_chat_member.id)
     if update.message.from_user.id == bot.id:
         update.message.delete()  # left_chat_member messages from kick
+
+
+def conversation(bot, update, chat_data):
+    if GREET_FROM_MEMBERS(update.message.text):
+        delete_job(chat_data)
+        not_greet_user(chat_data)
+
+
+def error(bot, update, error):
+    logger.warning('Update "{}" caused error "{}"'.format(update, error))
 
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_user))
-    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member, bye_user))
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members,
+                                  new_user,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member,
+                                  bye_user,
+                                  pass_chat_data=True))
+    dp.add_handler(MessageHandler(Filters.group,
+                                  conversation,
+                                  pass_chat_data=True))
+
+    # log all errors
+    dp.add_error_handler(error)
 
     # Ignore all messages until it is called
     #updater.start_polling(clean=True)
